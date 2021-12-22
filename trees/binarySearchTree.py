0834613db0666a4c1c768b1d8c9efeec2b212aff
sasikiran <sasikiranessamaraju@gmail.com>
2021-12-22 11:58:01 +0530

    add method to check if two trees are equal.

--- trees/binarySearchTree.py
+++ trees/binarySearchTree.py
@@ -100,3 +100,15 @@ class BinarySearchTree:
         while curr.right is not None:
             curr = curr.right
         return curr.data
+
+    def equals(self, other):
+        return self.__equals(self.root, other.root)
+
+    def __equals(self, root1, root2):
+        if root1 is None and root2 is None:
+            return True
+        
+        if root1 is not None and root2 is not None:
+            return root1.data == root2.data and self.__equals(root1.left, root2.left) and self.__equals(root1.right, root2.right)
+        
+        return False
